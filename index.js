2a49f34bbed6bf27ef1b0519c33794d11d3fd893
PlayerSamGo <zyxsamys@gmail.com>
2021-04-14 21:58:47 -0500

    Set up db connection

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-//const config = require('./scripts/setConfig.json');
+const config = require('./scripts/setConfig.json');
 
 //Load dependencies 
 const mongoose = require('mongoose');
-const port = 1200//config.port;
+const port = config.port;
 
 let app = require('./app');
 
@@ -11,6 +11,17 @@ let server = require('http').createServer(app); //Server with express
 
 //Init for start user creation before launch project 
 
-console.log("Inicio de app express exitosa");
 //Connection db
-//console.log("Conexion a base de datos exitosa");
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongodb_url, {useNewUrlParser: true, useUnifiedTopology: true },(err,res) =>{
+    if(err){
+      throw err;
+    }
+    else{
+      console.log("Conection sucessfull");
+      server.listen(port, function(){
+        console.log("Api Rest server listen on http://localhost:"+port);
+      })
+    }
+  });
+  
